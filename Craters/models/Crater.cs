2d0708088f8e638d449b4cac28047f111fa99f03
Kökényesi Andor <20.kokenyesi.andor@mechwart.com>
2025-04-14 12:41:56 +0200

    asd

--- Craters/models/Crater.cs
+++ Craters/models/Crater.cs
@@ -17,13 +17,9 @@ public class Crater
     public double YCord { get => yCord;  }
     public double Radius { get => radius;  }
     public string CraterName { get => craterName; }
-
     public double Circumference { get => 2 * Math.PI * radius;}
     public double Area { get => Math.PI * Math.Pow(radius, 2);}
 
-    public bool IsInside(Crater crater)
-    {
-        double distance = Math.Sqrt(Math.Pow(this.xCord - crater.xCord, 2) + Math.Pow(this.yCord - crater.yCord, 2));
-        return distance < (this.radius - crater.radius) ? true : false;
-    }
+    public bool IsInside(Crater crater) => Math.Sqrt(Math.Pow(this.xCord - crater.xCord, 2) + Math.Pow(this.yCord - crater.yCord, 2)) < (this.radius - crater.radius);
+
 }
